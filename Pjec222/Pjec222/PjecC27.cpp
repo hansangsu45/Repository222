b489d4d4fe04344c36b60ea2ce5be95a03e67b6b
hansangsu <hsangsu40@gmail.com>
2021-04-23 08:47:32 +0900

    원형 큐 구현

---
+++ Pjec222/Pjec222/PjecC27.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+#define NEXT(index,QSIZE) ((index+1)%QSIZE)
+
+typedef struct Queue {
+	int* buf;
+	int qsize;
+	int front;  //꺼낼 인덱스
+	int rear;  //보관할 인덱스
+	int count;
+}Queue;
+
+void InitQueue(Queue* queue, int qsize)
+{
+	queue->buf = new int[qsize];
+	queue->qsize = qsize;
+	queue->front = queue->rear = 0;
+	queue->count = 0;
+}
+
+int IsFull(Queue* queue)
+{
+	return queue->count == queue->qsize;
+}
+
+int IsEmpty(Queue* queue)
+{
+	return queue->count == 0;
+}
+
+void Enqueue(Queue* queue, int data)
+{
+	if (IsFull(queue)) {
+		cout << "큐가 꽉 찼음\n";
+		return;
+	}
+	queue->buf[queue->rear] = data;
+	queue->rear = NEXT(queue->rear, queue->qsize);
+	queue->count++;
+}
+
+int Dequeue(Queue* queue)
+{
+	int re = 0;
+	if (IsEmpty(queue))
+	{
+		cout << "큐가 비었음\n";
+		return re;
+	}
+	re = queue->buf[queue->front];
+	queue->front = NEXT(queue->front, queue->qsize);
+	cout << "dequeue: " << re;
+	queue->count--;
+	return re;
+}
+
+int main()
+{
+	int i, size;
+	int select = -1;
+
+	Queue queue;
+
+	cout << "큐의 크기를 입력하세요: ";
+	cin >> size;
+
+	InitQueue(&queue,size);
+
+	while (1)
+	{
+		cout << "1. Enqueue 2. Dequeue" << endl;
+		cout << ">>";
+		cin >> select;
+
+		switch(select)
+		{
+		case 1:
+			int value;
+			cout << "input data: ";
+			cin >> value;
+			Enqueue(&queue, value);
+			break;
+		case 2:
+			Dequeue(&queue);
+			break;
+		default:
+			break;
+		}
+		cout << endl;
+	}
+}
